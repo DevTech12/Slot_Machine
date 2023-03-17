447b585a4fb235364f8023f7685252c825cdb4de
DevTech12 <devshinde9359@gmail.com>
2023-03-17 16:58:44 +0530

    Created a Function name Slot Machine

--- main.py
+++ main.py
@@ -1,4 +1,48 @@
+import random
+
 MAX_LINES = 5
+MAX_BET = 100
+MIN_BET = 10
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def machine_spin(rows, cols, symbols):
+    all_symbol = []
+    for symbols, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbol.append(symbols)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbol = all_symbol[:]
+        for _ in range(rows):
+            value = random.choice(all_symbol)
+            current_symbol.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+
+        print()
+
 
 def deposite():
     while True:
@@ -26,9 +70,33 @@ def get_number_of_lines():
             print("Please enter a number.")
     return lines
 
+def get_bet():
+    while True:
+        amount = input("What would like to Bet? $ ")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}")
+        else:
+            print("Please enter a number.")
+    return amount
+ 
 def main():
     balance = deposite()
     lines = get_number_of_lines()
-    print(balance,lines)
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines 
 
-main()
+        if total_bet >= balance:
+            print(f"You not have enough amount to bet. Your Current Balance is ${balance}")
+        else:
+            break
+
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to : ${total_bet}.")
+    #print(balance,lines)
+    slots = machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
+main() 
